1ee0d02ca53dd34ab3246d94c8c34906d4cd9a73
Justin Henderson <jhenderson@tekrefresh.com>
2021-11-08 11:00:59 -0600

    Update gunicorn_config.py

--- data/gunicorn_config.py
+++ data/gunicorn_config.py
@@ -4,7 +4,7 @@ import multiprocessing
 import os
 
 os.environ["SERVER_SOFTWARE"] = "domain_stats"
-bind = "127.0.0.1:5730"
+bind = "0.0.0.0:5730"
 workers = 257
 threads = 384
 gunicorn.SERVER_SOFTWARE = 'domain_stats'
