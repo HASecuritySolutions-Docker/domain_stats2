cfb4bfd4b08d81bda4a3f369eb442bf87ab270ad
Justin Henderson <jhenderson@tekrefresh.com>
2021-11-08 11:02:08 -0600

    Update gunicorn_config.py

--- data/gunicorn_config.py
+++ data/gunicorn_config.py
@@ -5,7 +5,7 @@ import os
 
 os.environ["SERVER_SOFTWARE"] = "domain_stats"
 bind = "0.0.0.0:5730"
-workers = 257
-threads = 384
+workers = 64
+threads = 128
 gunicorn.SERVER_SOFTWARE = 'domain_stats'
     
